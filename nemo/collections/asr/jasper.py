9f00e98df3526d75b2c58c7ce84fdd34679cbbe2
Stanislav Beliaev <stasbelyaev96@gmail.com>
2020-04-16 14:41:56 -0700

    W&B Support, new speaker system and some refactoring
    Signed-off-by: Stanislav Beliaev <stasbelyaev96@gmail.com>

--- nemo/collections/asr/jasper.py
+++ nemo/collections/asr/jasper.py
@@ -222,9 +222,6 @@ class JasperEncoder(TrainableNM):
         if length is None:
             return s_input[-1]
 
-        # TODO: Delete.
-        assert torch.equal(length, length)
-
         return s_input[-1], length
 
 
